1e5b306cf67b5081d99179c4e0dbd60d2c97768d
Arthi <arthianeel2000@gmail.com>
2021-09-24 21:56:06 +0530

    notes app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MainComponent from './components/MainComponent';
+function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
